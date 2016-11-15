0c8ac9d0393de597abae79cd33578ac00d6ed1d6
whoblitzell <whoblitzell8@yahoo.com>
2016-11-15 00:35:51 -0500

    Attempt at fixing localhost - 127.0.0.1 callback error. Seems to be a configuration error causing 400 bad request on OAuth redirect...

--- TwitterFeedReader/Controllers/HomeController.cs
+++ TwitterFeedReader/Controllers/HomeController.cs
@@ -27,20 +27,20 @@ namespace TwitterFeedReader.Controllers
             ViewBag.Title = "Twitter Feed Reader Coding Challenge";
             return View();
         }
-
+        
         public ActionResult TwitterSignIn()
         {
             // Request an OAuth Request Token
             TwitterService twitterService = new TwitterService(OAuthAPIKey, OAuthAPISecret);
-
-            var url = Url.Action("TwitterSignInCallback", "Home", null, "http");
-            OAuthRequestToken requestToken = twitterService.GetRequestToken(url);
+            
+            //callback URL is locked to this application by the Twitter App itself
+            OAuthRequestToken requestToken = twitterService.GetRequestToken();
 
             // Redirect to the OAuth Authorization URL
             Uri uri = twitterService.GetAuthorizationUri(requestToken);
-            return new RedirectResult(uri.ToString(), false /*permanent*/);
+            return new RedirectResult(uri.ToString(), false);
         }
-
+        
         // This URL is registered as the application's callback under Twitter's DEV API
         public ActionResult TwitterSignInCallback(string oauth_token, string oauth_verifier)
         {
@@ -52,7 +52,7 @@ namespace TwitterFeedReader.Controllers
 
             // User authenticates using the Access Token
             twitterService.AuthenticateWith(accessToken.Token, accessToken.TokenSecret);
-            TwitterUser user = twitterService.VerifyCredentials(new VerifyCredentialsOptions() { SkipStatus = true });
+            TwitterUser user = twitterService.VerifyCredentials(new VerifyCredentialsOptions() { });
             bool successfulLogin = user != null && user.IsVerified == true;
 
             FormsAuthentication.SetAuthCookie(user.ScreenName, false);
